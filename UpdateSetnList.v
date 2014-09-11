ff541116f4b77194f423c1e79ab2def246d55c29
Sorawit Suriyakarn <sorawit@mit.edu>
2014-09-11 18:51:41 -0400

    rewrite update

--- UpdateSetnList.v
+++ UpdateSetnList.v
@@ -32,6 +32,35 @@ Definition similar (l: list) (s: set): Prop :=
     forall x: X, InList l x <-> InSet s x.
 
 (** Proof **)
+Lemma in_list_helper: forall (xs: list) (x x0: X),
+    InList xs x0 -> InList (cons x xs) x0.
+Proof.
+    intros.
+    simpl. right. intuition.
+Qed.
+Theorem swits_proof: forall (l: list) (s: set) (f: X -> bool) (u: X -> X),
+    similar l s -> similar (UpdateList l f u) (UpdateSet s f u).
+Proof.
+    unfold similar in *.
+    induction l as [| x xs].
+    intros. split.
+        simpl. intuition.
+        intro I. destruct I.
+            apply H in H0. unfold InList in H0. intuition.
+            apply H in H0. unfold InList in H0. intuition.
+    intros. split.
+        simpl. intros. destruct H0.
+            case_eq (f x).
+                intros. rewrite H1 in H0. rewrite H0. constructor.
+                    apply H. simpl. left. trivial.
+                    assumption.
+                intros. constructor. 
+                    apply H. constructor. rewrite H1 in H0. assumption.
+                    rewrite H1 in H0. rewrite H0. assumption.
+            case_eq (f x0).
+                intros. 
+                    
+(**
 Lemma similar_with_null: forall s: set, similar nil EmptySet.
 Proof.
     intros s.
